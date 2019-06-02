2c91a10e5dff7384c4edab18b475fdf50d466d3a
newbalancem5 <newbalancem5@gmail.com>
2019-06-02 22:04:41 +0300

    Update Duration.dart

--- lib/Duration.dart
+++ lib/Duration.dart
@@ -15,15 +15,28 @@ static const int secondPerDay = secondPerHour * hoursPerDay;
 
 static const TimeDuration zero = TimeDuration(seconds : 0);
 
+final int _duration;
+
 const TimeDuration (
 {
   int hour = 0,
   int minutes = 0,
   int secons = 0,
   int microseconds = 0,
-});
+}):this._microseconds(secondPerDay*minutesPerDay);
 
 
+const TimeDuration._microseconds(this._duration);
 
+TimeDuration operator +(TimeDuration other){
+  return TimeDuration._microseconds(_duration + other._duration);
+}
 
+TimeDuration operator -(TimeDuration other){
+  return TimeDuration._microseconds(_duration - other._duration);
+}
+
+TimeDuration operator *(num factor){
+  return TimeDuration._microseconds(_duration * factor);
+}
 }
